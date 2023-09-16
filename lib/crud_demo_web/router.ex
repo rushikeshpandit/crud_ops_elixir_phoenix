c6baa519f810a1ad727ca90a613e2b883a3a9dcd
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-09-16 21:17:49 +0530

    - Added "/Persons" resource

--- lib/crud_demo_web/router.ex
+++ lib/crud_demo_web/router.ex
@@ -18,6 +18,7 @@ defmodule CrudDemoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/persons", PersonController
   end
 
   # Other scopes may use custom stacks.
